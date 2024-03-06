13e5e3ec53d6ec96b48a05a687348e958412c316
Riya Saryam <151647644+iamriyasaryam@users.noreply.github.com>
2024-03-06 22:24:04 +0530

    Add files via upload

---
+++ defenging.java
@@ -0,0 +1,11 @@
+public class defenging {
+    public static void main(String args[]){
+        String address = "1.1.1.1";
+        System.out.println(defangIPaddr(address));
+    }
+    
+     public static String defangIPaddr(String address) {
+         return address.replace(".", "[.]");
+    }
+
+}
